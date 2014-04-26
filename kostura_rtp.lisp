d8551feda8e47e282f16656c807c720775f018fd
Josh K <kosturja@gmail.com>
2014-04-26 09:35:40 -0400

    Stuff

--- kostura_rtp.lisp
+++ kostura_rtp.lisp
@@ -2,3 +2,39 @@
 ;; Joshua Kostura
 ;; University of Maine
 ;; COS 570 Spring 2014
+
+(defvar *Clause_list*)
+
+(defun myfun ()
+  (+ 5 5))
+
+;;Determines if a clause starts with an OR
+(defun or_clause (clause)
+  (if (equal (car clause) 'or) t
+      nil))
+
+;;Determines if a clause starts with an NOT
+
+(defun not_clause (clause)
+  (if (equal (car clause) 'not) t
+      nil))
+
+
+;;A function to negate a clause
+;; Creates a negated clause if the clause passed is true
+;; Creates a true clause if the clause passed is negative.
+
+(defun negate (clause)
+  (if (not_clause clause) (cdr clause)
+      (append (list 'not) (list clause))))
+
+
+(defun handle_or_clause (clause)
+  ;;Designed to take an OR clause, and seperate each clause
+  (if (not (or_clause clause)) "Error No OR detected"
+      (let (item cadr clause)
+	(loop for item in clause)
+		 (push item *Clause_list*))))
+
+
+	       
