0a8562224bcd19b0c90ebfeeba57537099f73f3e
Josh K <kosturja@gmail.com>
2014-04-30 00:11:46 -0400

    Submission

--- kostura_rtp.lisp
+++ kostura_rtp.lisp
@@ -6,7 +6,7 @@
 (defvar *Clause_list* '())
 (load "unify.lisp")
 (load "axioms.lisp")
-
+(defvar *Proof_list* nil)
 
 ;;Determines if a clause starts with an OR
 (defun or_clause (clause)
@@ -86,7 +86,8 @@
 	 ;do (print item)
 	 if (equal clause item)
 	 do (progn
-	      (print (format t " ~S was resolved with ~S from ~S"(negate clause) item axiom_list))
+	      (print (format t " ~S was resolved with ~S from ~S"(negate clause)
+			     item axiom_list))
 	      (return cnt2))
 	 else do (progn
 		   ;(print cnt)
@@ -99,7 +100,8 @@
   ;; returns the location of the resolvent in KB
   ;; Returns pair if the resolvent is part of a larger axiom
   ;; the first index is the axiom, the second index is the location in the axiom
-  ;; since we add one to our counter before we look at an axiom, we must subtract one
+  ;; since we add one to our counter before we look at an axiom,
+  ;; we must subtract one
   ;; form the first index just before we return.
   (let* ((cnt 0)
 	(clause (negate clause)))
@@ -113,7 +115,8 @@
 			(return-from my_find (list (- cnt 1) inner)))))
        else if (equal clause item)
 	 do (progn
-	      (print (format t "~S was resolved with ~S from axiom ~S from the knowledge base." (negate clause) clause cnt))
+	      (print (format t "~S was resolved with ~S from axiom ~S from the
+knowledge base." (negate clause) clause cnt))
 	      (return-from my_find  (list (- cnt 1) 0))))))
 
 
@@ -160,16 +163,17 @@
 	 do (print item)
 	 do (print "==========")
 	 do (setf newest_clause item)
-					; Pick the first clause to resolve from proof_list
-					; Then we negate it and see if it resides in the axioms
+	       ; Pick the first clause to resolve from proof_list
+		; Then we negate it and see if it resides in the axioms
 	 do (setf location (my_find (negate newest_clause) *axioms*))
 	 if (not (null location))
-	 do (setf newer_clause (resolve (negate newest_clause) axiom_list location))
+	 do (setf newer_clause (resolve (negate newest_clause) axiom_list
+					location))
 	 ;do (print newer_clause)
 	 do (setf prooflist newer_clause)
 	 do (print prooflist)
 	 )))
-					; store its location in the axiom set in location
+			     ; store its location in the axiom set in location
 
 
 ;; Accepts the negated clause to be proven.
@@ -187,7 +191,7 @@
 		 (newer_clause '())
 		 (temp '())
 		 (temp2 '()))
-					;(print (format t "Clause:~S Prflist:~S" clause prflist))
+		 ;(print (format t "Clause:~S Prflist:~S" clause prflist))
 	     (cond
 	       ;; This was designed for recursion call, and passing the option
 	       ;; parameter which would be addition things we need to prove
@@ -197,7 +201,8 @@
 					;(print (format t "There not null"))
 		(setf prooflist (append (list 'or clause prooflist))))
 	       (t
-		;; This is what we want to prove, basically the clause we are passed
+		;; This is what we want to prove,
+		;; basically the clause we are passed
 		(setf prooflist (append clause))))
 	     (cond
 	       ;; If our clause is compound we start by proving the last item.
@@ -206,17 +211,19 @@
 		(print (format t "We are trying to resolve :~S" temp))
 		;; We need to maintain the rest of the orignal proof list
 		;; We append this later at pass it to the recursive call of prove
-		(setf temp2 (remove_and_or  (reverse (cdr (reverse prooflist))))))
+		(setf temp2 (remove_and_or  (reverse (cdr
+						      (reverse prooflist))))))
 	       (t
 		(setf temp prooflist)))
-					;(print (format t "The rest of prooflist:~S "temp2))
-					;(print (format t "Temp before we resolve:~S" temp))
+		;(print (format t "The rest of prooflist:~S "temp2))
+		;(print (format t "Temp before we resolve:~S" temp))
 	     ;; Resolve are clause
-	     (setf newer_clause (resolve temp axiom_list (my_find temp axiom_list)))
+	     (setf newer_clause (resolve temp axiom_list (my_find temp
+								  axiom_list)))
 	     (cond
 	       ;; Remove unnecessary or
 	       ((not (null newer_clause))
-					;(print (format t "Removeing things :~S"newer_clause))
+			 ;(print (format t "Removeing things :~S"newer_clause))
 		(setf new_clause (remove_and_or newer_clause))
 		(cond
 		  ;; Here we append the results of resolving our clause
@@ -224,11 +231,11 @@
 		  ((not (null temp2))
 		   (setf new_clause (append (list 'or temp2 new_clause))))))
 	       (t
-					;(print (format t "Newer_clause was nil:~S" temp2))
+			 ;(print (format t "Newer_clause was nil:~S" temp2))
 		;; If the resolve funciton returned nul, because a single clause
 		;; resolved with a single clause we just pass the rest of the
 		;; original prooflist
 		(setf new_clause  temp2)))
-					;(print (format t "Newer clause: ~S" newer_clause))
-					;(print (format t "New Clause: ~S" new_clause))
+		;(print (format t "Newer clause: ~S" newer_clause))
+		       ;(print (format t "New Clause: ~S" new_clause))
 	     (prove new_clause axiom_list )))))
